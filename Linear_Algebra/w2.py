d02790b0195ad0c0efdbeaf345b3224bbac7f36c
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2023-02-10 19:35:25 +0530

    remember to use .copy when swapping

--- Linear_Algebra/w2.py
+++ Linear_Algebra/w2.py
@@ -1,2 +1,24 @@
 # %%
-import
+import numpy as np
+
+
+# %%
+def SwapRows(M, row_num_1, row_num_2):
+    M_new = M.copy()
+    # exchange row_num_1 and row_num_2 of the matrix M_new
+    temp = M_new[row_num_1].copy()
+    M_new[row_num_1] = M_new[row_num_2]
+    M_new[row_num_2] = temp
+    return M_new
+
+
+# %%
+A_test = np.array([
+    [1, -2, 3, -4],
+    [-5, 6, -7, 8],
+    [-4, 3, -2, 1],
+    [8, -7, 6, -5]
+], dtype=np.dtype(float))
+
+# %%
+print(SwapRows(A_test, 0, 2))
