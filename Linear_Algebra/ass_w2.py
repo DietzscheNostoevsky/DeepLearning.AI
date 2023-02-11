ab7d27324d35975fc31bb0fef803c71b047952e2
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2023-02-11 13:57:56 +0530

    Update ass_w2.py

---
+++ Linear_Algebra/ass_w2.py
@@ -0,0 +1,48 @@
+def augmented_to_ref(A, b):
+    ### START CODE HERE ###
+    # stack horizontally matrix A and vector b, which needs to be reshaped as a vector (4, 1)
+    A_system = None
+
+    # swap row 0 and row 1 of matrix A_system (remember that indexing in NumPy array starts from 0)
+    A_ref = None
+
+    # multiply row 0 of the new matrix A_ref by -2 and add it to the row 1
+    A_ref = None
+
+    # add row 0 of the new matrix A_ref to the row 2, replacing row 2
+    A_ref = None
+
+    # multiply row 0 of the new matrix A_ref by -1 and add it to the row 3
+    A_ref = None
+
+    # add row 2 of the new matrix A_ref to the row 3, replacing row 3
+    A_ref = None
+
+    # swap row 1 and 3 of the new matrix A_ref
+    A_ref = None
+
+    # add row 2 of the new matrix A_ref to the row 3, replacing row 3
+    A_ref = None
+
+    # multiply row 1 of the new matrix A_ref by -4 and add it to the row 2
+    A_ref = None
+
+    # add row 1 of the new matrix A_ref to the row 3, replacing row 3
+    A_ref = None
+
+    # multiply row 3 of the new matrix A_ref by 2 and add it to the row 2
+    A_ref = None
+
+    # multiply row 2 of the new matrix A_ref by -8 and add it to the row 3
+    A_ref = None
+
+    # multiply row 3 of the new matrix A_ref by -1/17
+    A_ref = None
+    ### END CODE HERE ###
+
+    return A_ref
+
+
+A_ref = augmented_to_ref(A, b)
+
+print(A_ref)
